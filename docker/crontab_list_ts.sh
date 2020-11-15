633e41a37ce995697d820049229429bc36bcd38d
deepdark111 <67956668+deepdark111@users.noreply.github.com>
2020-11-15 15:18:02 +0800

    Update crontab_list_ts.sh
    修复养猪cron时间

--- docker/crontab_list_ts.sh
+++ docker/crontab_list_ts.sh
@@ -52,4 +52,4 @@
 # 天天提鹅
 18 */3 * * * node /scripts/jd_daily_egg.js |ts >> /scripts/logs/jd_daily_egg.log 2>&1
 # 金融养猪
-12 13 * * * node /scripts/jd_pigPet.js |ts >> /scripts/logs/jd_pigPet.log 2>&1
+12 * * * * node /scripts/jd_pigPet.js |ts >> /scripts/logs/jd_pigPet.log 2>&1
