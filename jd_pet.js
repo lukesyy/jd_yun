61f7a341bc42b2994bb643bd3139851ba725b626
lxk0301 <778732665@qq.com>
2020-11-23 20:04:14 +0800

    Update jd_pet.js

--- jd_pet.js
+++ jd_pet.js
@@ -38,7 +38,7 @@ let message = '', subTitle = '', option = {};
 let jdNotify = false;//是否关闭通知，false打开通知推送，true关闭通知推送
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
 let goodsUrl = '', taskInfoKey = [];
-let randomCount = 20;
+let randomCount = $.isNode() ? 20 : 5;
 !(async () => {
   await requireConfig();
   if (!cookiesArr[0]) {
