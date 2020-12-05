9ff7052bb40ed561c6b267d46cc6d757675857ef
lxk0301 <778732665@qq.com>
2020-12-05 14:46:18 +0800

    宠汪汪赛跑增加控制参加多少人的赛跑比赛，默认还是2人，可通过BoxJs或者环境变量/secret进行控制。环境变量名称JOY_TEAM_LEVEL

--- jd_joy.js
+++ jd_joy.js
@@ -2,7 +2,7 @@
 jd宠汪汪 搬的https://github.com/uniqueque/QuantumultX/blob/4c1572d93d4d4f883f483f907120a75d925a693e/Script/jd_joy.js
 脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
 IOS用户支持京东双账号,NodeJs用户支持N个京东账号
-更新时间：2020-11-03
+更新时间：2020-12-05
 建议先凌晨0点运行jd_joy.js脚本获取狗粮后，再运行此脚本(jd_joy_steal.js)可偷好友积分，6点运行可偷好友狗粮
 feedCount:自定义 每次喂养数量; 等级只和喂养次数有关，与数量无关
 推荐每次投喂10个，积累狗粮，然后去聚宝盆赌每小时的幸运奖，据观察，投入3000-6000中奖概率大，超过7000基本上注定亏本，即使是第一名
@@ -37,6 +37,7 @@ if ($.isNode()) {
 }
 let message = '', subTitle = '';
 let FEED_NUM = ($.getdata('joyFeedCount') * 1) || 10;   //每次喂养数量 [10,20,40,80]
+let teamLevel = 2;//参加多少人的赛跑比赛，默认是双人赛跑，可选2，10,50。其他不可选，其中2代表参加双人PK赛，10代表参加10人突围赛，50代表参加50人挑战赛
 //是否参加宠汪汪双人赛跑（据目前观察，参加双人赛跑不消耗狗粮,如需参加其他多人赛跑，请关闭）
 // 默认 'true' 参加双人赛跑，如需关闭 ，请改成 'false';
 let joyRunFlag = true;
@@ -137,11 +138,12 @@ async function joinTwoPeopleRun() {
       let raceUsers = $.petRaceResult.data.raceUsers;
       console.log(`赛跑状态：${petRaceResult}\n`);
       if (petRaceResult === 'not_participate') {
-        console.log('暂未参赛，现在为您参加双人赛跑');
-        await runMatch(2);
+        teamLevel = $.isNode() ? (process.env.JOY_TEAM_LEVEL ? process.env.JOY_TEAM_LEVEL : teamLevel) : ($.getdata('JOY_TEAM_LEVEL') ? $.getdata('JOY_TEAM_LEVEL') : teamLevel);
+        console.log(`暂未参赛，现在为您参加${teamLevel}人赛跑`);
+        await runMatch(teamLevel * 1);
         if ($.runMatchResult.success) {
-          console.log(`双人赛跑参加成功\n`);
-          message += `双人赛跑：成功参加\n`;
+          console.log(`${teamLevel}人赛跑参加成功\n`);
+          message += `${teamLevel}人赛跑：成功参加\n`;
           await getPetRace();
           petRaceResult = $.petRaceResult.data.petRaceResult;
           raceUsers = $.petRaceResult.data.raceUsers;
@@ -160,7 +162,8 @@ async function joinTwoPeopleRun() {
         await receiveJoyRunAward();
         console.log(`领取赛跑奖励结果：${JSON.stringify($.receiveJoyRunAwardRes)}`)
         if ($.receiveJoyRunAwardRes.success) {
-          $.msg($.name, '双人赛跑取得获胜', `【京东账号${$.index}】${$.nickName}\n太棒了,恭喜您获得300积分奖励`)
+          $.msg($.name, '', `【京东账号${$.index}】${$.nickName}\n太棒了,${teamLevel}人赛跑取得获胜\n恭喜您已获得相应的积分奖励`);
+          await notify.sendNotify(`${$.name} - 京东账号${$.index} - ${$.nickName}`, `${teamLevel}人赛跑取得获胜\n恭喜您已获得相应的积分奖励`)
         }
       }
       if (petRaceResult === 'participate') {
