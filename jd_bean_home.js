b82ed461b93ad9792b49ab546151e058a9c55ed0
shylocks <shylocksyang@gmail.com>
2020-12-11 07:26:00 +0800

    修复开团问题

--- jd_bean_home.js
+++ jd_bean_home.js
@@ -42,7 +42,6 @@ const JD_API_HOST = 'https://api.m.jd.com/';
 !(async () => {
   $.newShareCodes = []
   await getAuthorShareCode()
-  return
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/', {"open-url": "https://bean.m.jd.com/"});
     return;
