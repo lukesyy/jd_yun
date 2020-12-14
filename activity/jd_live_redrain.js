3ce4f82a0461de1ad26cdeca2024e0351036ddf5
shylocks <shylocksyang@gmail.com>
2020-12-14 19:12:31 +0800

    关闭通知

--- activity/jd_live_redrain.js
+++ activity/jd_live_redrain.js
@@ -25,7 +25,7 @@ const $ = new Env('直播红包雨');
 const notify = $.isNode() ? require('../sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('../jdCookie.js') : '';
-let jdNotify = false;//是否关闭通知，false打开通知推送，true关闭通知推送
+let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭通知推送
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message;
 if ($.isNode()) {
