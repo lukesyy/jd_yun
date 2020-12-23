da73c1b00728486a7fac044f41a6cd4497689d33
Akira <e.akimoto.akira@gmail.com>
2020-12-23 11:16:08 +0800

    bug fix
    因为之前测试时候日志输出未设置ts，所以$1没问题，默认所有日志有|ts输出，所以需要改取 $4,$5也取是担心中间会有多的空格影响分列

--- docker/proc_file.sh
+++ docker/proc_file.sh
@@ -14,22 +14,22 @@ jdpet="${logdDir}/jd_pet.log"
 
 echo -e >$sharecodeFile
 
-sed -n '/京东赚赚好友互助码】.*/'p $jdzzFile | awk '{print $1}' | sort | uniq >>$sharecodeFile
+sed -n '/京东赚赚好友互助码】.*/'p $jdzzFile | awk '{print $4,$5}' | sort | uniq >>$sharecodeFile
 echo "提取京东赚赚助力码完成"
 
-sed -n '/东东工厂好友互助码】.*/'p $jdfactoryFile | awk '{print $1}' | sort | uniq >>$sharecodeFile
+sed -n '/东东工厂好友互助码】.*/'p $jdfactoryFile | awk '{print $4,$5}' | sort | uniq >>$sharecodeFile
 echo "提取东东工厂助力码完成"
 
-sed -n '/京喜工厂好友互助码.*/'p $jxFactoryFile | awk '{print $1}' | sort | uniq >>$sharecodeFile
+sed -n '/京喜工厂好友互助码.*/'p $jxFactoryFile | awk '{print $4,$5}' | sort | uniq >>$sharecodeFile
 echo "提取京喜工厂助力码完成"
 
-sed -n '/京东种豆得豆好友互助码】.*/'p $plantBean | awk '{print $1}' | sort | uniq >>$sharecodeFile
+sed -n '/京东种豆得豆好友互助码】.*/'p $plantBean | awk '{print $4,$5}' | sort | uniq >>$sharecodeFile
 echo "提取京东种豆助力码完成"
 
-sed -n '/东东农场好友互助码】.*/'p $jdfruit | awk '{print $1}' | sort | uniq >>$sharecodeFile
+sed -n '/东东农场好友互助码】.*/'p $jdfruit | awk '{print $4,$5}' | sort | uniq >>$sharecodeFile
 echo "提取京东农场助力码完成"
 
-sed -n '/东东萌宠好友互助码】.*/'p $jdpet | awk '{print $1}' | sort | uniq >>$sharecodeFile
+sed -n '/东东萌宠好友互助码】.*/'p $jdpet | awk '{print $4,$5}' | sort | uniq >>$sharecodeFile
 echo "提取东东萌宠助力码完成"
 
 echo "==========================================================================="
