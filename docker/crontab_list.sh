249c3e8cdd205256299476941a63a7f0c8c90654
lxk0301 <778732665@qq.com>
2021-01-01 15:57:39 +0800

    健康抽奖机还能用

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -4,7 +4,8 @@
 50 23 */3 * * rm -rf /scripts/logs/*.log
 
 ##############短期活动##############
-
+# 健康抽奖机(2020.12.31活动过期)
+10 0 1-31 12 * node /scripts/jd_health.js >> /scripts/logs/jd_health.log 2>&1
 ##############长期活动##############
 # 签到
 0 0,18 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
