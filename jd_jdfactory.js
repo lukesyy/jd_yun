daa176d3f8b503b0a84edd36864c897420eb0dbb
lxk0301 <778732665@qq.com>
2021-01-01 17:17:34 +0800

    东东工厂  做任务增加延迟时间

--- jd_jdfactory.js
+++ jd_jdfactory.js
@@ -355,7 +355,8 @@ async function doTask() {
 
 //领取做完任务的奖励
 function jdfactory_collectScore(taskToken) {
-  return new Promise(resolve => {
+  return new Promise(async resolve => {
+    await $.wait(1000);
     $.post(taskPostUrl("jdfactory_collectScore", { taskToken }, "jdfactory_collectScore"), async (err, resp, data) => {
       try {
         if (err) {
