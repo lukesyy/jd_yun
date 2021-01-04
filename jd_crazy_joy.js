f44b13ce8ff73aa40ca4e454209dfd491f8f72e7
lxk0301 <778732665@qq.com>
2021-01-04 12:35:46 +0800

    东东工厂添加判断，减少部分用户获取的数据不全导致脚本出错

--- jd_crazy_joy.js
+++ jd_crazy_joy.js
@@ -666,7 +666,7 @@ function readShareCode() {
           console.log(`${$.name} API请求失败，请检查网路重试`)
         } else {
           if (data) {
-            console.log(`随机取${randomCount}个码放到您固定的互助码后面`)
+            console.log(`随机取${randomCount}个码放到您固定的互助码后面(不影响已有固定互助)`)
             data = JSON.parse(data);
           }
         }
