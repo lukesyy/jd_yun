f53acf3aa455d2dc6f249ba2c1cfb5f1a19a3439
lxk0301 <778732665@qq.com>
2021-01-05 17:39:33 +0800

    改为Promise.all

--- sendNotify.js
+++ sendNotify.js
@@ -2,7 +2,7 @@
  * @Author: lxk0301 https://github.com/lxk0301 
  * @Date: 2020-08-19 16:12:40 
  * @Last Modified by: lxk0301
- * @Last Modified time: 2020-12-21 13:52:54
+ * @Last Modified time: 2021-1-5 17:52:54
  */
 const querystring = require("querystring");
 const $ = new Env();
@@ -132,17 +132,21 @@ if (process.env.PUSH_PLUS_USER) {
 
 async function sendNotify(text, desp, params = {}) {
   //提供7种通知
-  await serverNotify(text, desp);//微信server酱
-  await pushPlusNotify(text, desp);//pushplus(推送加)
+  await Promise.all([
+    serverNotify(text, desp),//微信server酱
+    pushPlusNotify(text, desp)//pushplus(推送加)
+  ])
   //由于上述两种微信通知需点击进去才能查看到详情，故text(标题内容)携带了账号序号以及昵称信息，方便不点击也可知道是哪个京东哪个活动
   text = text.match(/.*?(?=\s?-)/g) ? text.match(/.*?(?=\s?-)/g)[0] : text;
-  await BarkNotify(text, desp, params);//iOS Bark APP
-  await tgBotNotify(text, desp);//telegram 机器人
-  await ddBotNotify(text, desp);//钉钉机器人
-  await qywxBotNotify(text, desp); //企业微信机器人
-  await qywxamNotify(text, desp); //企业微信应用消息推送
-  await iGotNotify(text, desp, params);//iGot
-  await CoolPush(text, desp);//QQ酷推
+  await Promise.all([
+    BarkNotify(text, desp, params),//iOS Bark APP
+    tgBotNotify(text, desp),//telegram 机器人
+    ddBotNotify(text, desp),//钉钉机器人
+    qywxBotNotify(text, desp), //企业微信机器人
+    qywxamNotify(text, desp), //企业微信应用消息推送
+    iGotNotify(text, desp, params),//iGot
+    CoolPush(text, desp)//QQ酷推
+  ])
 }
 
 function serverNotify(text, desp, timeout = 2100) {
