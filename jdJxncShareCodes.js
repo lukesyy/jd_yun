17485d3eee0fa57ef914864aa26238a5a990ca93
guyuexuan <2651919937@qq.com>
2021-01-10 23:13:47 +0800

    替换环境变量名为  JXNC_SHARECODES （原 JXNCSHARECODES）

--- jdJxncShareCodes.js
+++ jdJxncShareCodes.js
@@ -13,15 +13,15 @@ let JxncShareCodes = [
   '',//账号二的好友shareCode，不同好友中间用@符号隔开
 ]
 // 判断github action里面是否有京喜农场助力码
-if (process.env.JXNCSHARECODES) {
-  if (process.env.JXNCSHARECODES.indexOf('&') > -1) {
+if (process.env.JXNC_SHARECODES) {
+  if (process.env.JXNC_SHARECODES.indexOf('&') > -1) {
     console.log(`您的京喜农场助力码选择的是用&隔开\n`)
-    JxncShareCodes = process.env.JXNCSHARECODES.split('&');
-  } else if (process.env.JXNCSHARECODES.indexOf('\n') > -1) {
+    JxncShareCodes = process.env.JXNC_SHARECODES.split('&');
+  } else if (process.env.JXNC_SHARECODES.indexOf('\n') > -1) {
     console.log(`您的京喜农场助力码选择的是用换行隔开\n`)
-    JxncShareCodes = process.env.JXNCSHARECODES.split('\n');
+    JxncShareCodes = process.env.JXNC_SHARECODES.split('\n');
   } else {
-    JxncShareCodes = process.env.JXNCSHARECODES.split();
+    JxncShareCodes = process.env.JXNC_SHARECODES.split();
   }
 } else if (process.env.JD_COOKIE) {
   // console.log(`由于您secret里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
