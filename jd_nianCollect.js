3056271a08c0aa21ff7f58364ec758d7ed3497e8
LXK9301 <72646373+LXK9301@users.noreply.github.com>
2021-01-18 08:34:03 +0800

    Update jd_nianCollect.js

--- jd_nianCollect.js
+++ jd_nianCollect.js
@@ -3,7 +3,7 @@
 仅仅是收集一下京东炸年兽领爆竹活动每秒产生的爆竹🧨
 
 每小时的第20分运行一次
-20 * * * * https://raw.githubusercontent.com/LXK9301/jd_scripts/master/jd_niancollect.js
+20 * * * * https://raw.githubusercontent.com/LXK9301/jd_scripts/master/jd_nianCollect.js
  */
 const $ = new Env('京东炸年兽领爆竹🧨');
 //Node.js用户请在jdCookie.js处填写京东ck;
