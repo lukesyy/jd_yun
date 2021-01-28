b2305350edb405c307a24f6a1ff76bd12d502bd4
danwangshimoluo <evislcq@foxmail.com>
2021-01-28 20:17:43 +0800

    淦！

--- jd_nian_ar.js
+++ jd_nian_ar.js
@@ -34,7 +34,7 @@ if ($.isNode()) {
     cookiesArr.push(jdCookieNode[item])
   })
   if (process.env.JD_DEBUG && process.env.JD_DEBUG === 'false') console.log = () => {};
-  if(JSON.stringify(process.env).indexOf('GITHUB')>-1) process.exit(0)
+//  if(JSON.stringify(process.env).indexOf('GITHUB')>-1) process.exit(0)
 } else {
   let cookiesData = $.getdata('CookiesJD') || "[]";
   cookiesData = jsonParse(cookiesData);
