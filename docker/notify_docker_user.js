6a11bcba4bb6183b529c1151be5d6fe561590de1
zero205 <1184642956@qq.com>
2021-02-18 16:21:51 +0800

    删除docker相关脚本

--- docker/notify_docker_user.js
+++
@@ -1,9 +0,0 @@
-const notify = require('../sendNotify');
-
-function image_update_notify() {
-    if (process.env.NOTIFY_CONTENT) {
-        notify.sendNotify("⚠️Docker镜像版本更新通知⚠️", process.env.NOTIFY_CONTENT)
-    }
-}
-
-image_update_notify();
