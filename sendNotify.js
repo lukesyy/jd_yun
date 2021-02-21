e988afbc60e16fefaf4bf42a77465232a5e33365
zero205 <1184642956@qq.com>
2021-02-21 18:55:39 +0800

    鉴于LXK大佬仓库私有化，如若无人分享脚本，本仓库有可能也会就此停更，请知悉

--- sendNotify.js
+++ sendNotify.js
@@ -135,7 +135,7 @@ if (process.env.PUSH_PLUS_USER) {
 
 async function sendNotify(text, desp, params = {}) {
   //提供6种通知
-  desp += `\n本脚本开源免费使用 By：https://github.com/LXK9301/jd_scripts`;
+  desp += `\n鉴于LXK大佬仓库私有化，如若无人分享脚本，本仓库有可能也会就此停更，请知悉\n本脚本开源免费使用 By：https://github.com/LXK9301/jd_scripts`;
   await Promise.all([
     serverNotify(text, desp),//微信server酱
     pushPlusNotify(text, desp)//pushplus(推送加)
