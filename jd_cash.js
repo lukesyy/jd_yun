30f21084dd18be2f41c96a22d6d08b2bb720351a
zero205 <1184642956@qq.com>
2021-03-05 09:35:03 +0800

    fix

--- jd_cash.js
+++ jd_cash.js
@@ -363,25 +363,24 @@ function taskUrl(functionId, body = {}) {
   }
 }
 
-// function getAuthorShareCode() {
-//  return new Promise(resolve => {
-//    $.get({url: "https://gitee.com/shylocks/updateTeam/raw/main/jd_cash.json",headers:{
-//        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
-//      }}, async (err, resp, data) => {
-//      $.authorCode = [];
-//      try {
-//        if (err) {
-//        } else {
-//          $.authorCode = JSON.parse(data)
-//        }
-//      } catch (e) {
-//       $.logErr(e, resp)
-//      } finally {
-//        resolve();
-//      }
-//    })
-//  })
-// }
+ function getAuthorShareCode() {
+  return new Promise(resolve => {
+    $.get({url: "https://gitee.com/shylocks/updateTeam",headers:{
+        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
+      }}, async (err, resp, data) => {
+      $.authorCode = [];
+      try {
+        if (err) {
+        } else {
+          $.authorCode = JSON.parse(data)
+        }
+      } catch (e) {
+       $.logErr(e, resp)
+      } finally {
+        resolve();
+      }
+    })
+  }) 
 
 function TotalBean() {
   return new Promise(async resolve => {
