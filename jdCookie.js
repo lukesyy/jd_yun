8c45b01651a128d2976565b16a232d0919215203
zero205 <1184642956@qq.com>
2021-03-07 22:18:41 +0800

    fix

--- jdCookie.js
+++ jdCookie.js
@@ -21,8 +21,8 @@ if (process.env.JD_COOKIE) {
 if (JSON.stringify(process.env).indexOf('GITHUB')>-1) {
   console.log(`请勿使用github action运行此脚本,无论你是从你自己的私库还是其他哪里拉取的源代码，都会导致我被封号\n`);
   !(async () => {
-    await require('./sendNotify').sendNotify('提醒', `请勿使用github action、滥用github资源会封我仓库以及账号`)
-    await process.exit(0);
+   // await require('./sendNotify').sendNotify('提醒', `请勿使用github action、滥用github资源会封我仓库以及账号`)
+   // await process.exit(0);
   })()
 }
 CookieJDs = [...new Set(CookieJDs.filter(item => !!item))]
