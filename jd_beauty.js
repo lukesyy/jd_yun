b6c77681362cd713170ac6ea5a92d5137c4fdef2
zero205 <1184642956@qq.com>
2021-03-07 21:38:25 +0800

    修复读取账号错误问题

--- jd_beauty.js
+++ jd_beauty.js
@@ -10,7 +10,7 @@ const $ = new Env('美丽研究院');
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
-const WebSocket = $.isNode() ? require('websocket').w3cwebsocket: SockJS;
+//const WebSocket = $.isNode() ? require('websocket').w3cwebsocket: SockJS;
 let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭通知推送
 const randomCount = $.isNode() ? 0 : 5;
 const bean = 500
