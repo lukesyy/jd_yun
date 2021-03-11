47ce6ac5be8b560977ada735c8853a4fe8777535
zero205 <1184642956@qq.com>
2021-03-11 10:02:54 +0800

    fix

--- jdCookie.js
+++ jdCookie.js
@@ -18,6 +18,7 @@ if (process.env.JD_COOKIE) {
     CookieJDs = [process.env.JD_COOKIE];
   }
 }
+/*
 if (JSON.stringify(process.env).indexOf('GITHUB')>-1) {
   console.log(`请勿使用github action运行此脚本,无论你是从你自己的私库还是其他哪里拉取的源代码，都会导致我被封号\n`);
   !(async () => {
@@ -25,6 +26,7 @@ if (JSON.stringify(process.env).indexOf('GITHUB')>-1) {
    // await process.exit(0);
   })()
 }
+*/
 CookieJDs = [...new Set(CookieJDs.filter(item => !!item))]
 console.log(`\n====================共有${CookieJDs.length}个京东账号Cookie=========\n`);
 console.log(`==================脚本执行- 北京时间(UTC+8)：${new Date(new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*60*60*1000).toLocaleString()}=====================\n`)
