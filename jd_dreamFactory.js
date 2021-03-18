56c742db4afd827e9f975305c86da1b7020880cf
zero205 <1184642956@qq.com>
2021-03-18 10:56:04 +0800

    fix

--- jd_dreamFactory.js
+++ jd_dreamFactory.js
@@ -14,7 +14,7 @@ const helpAuthor = true;
 const notify = $.isNode() ? require('./sendNotify') : '';
 let jdNotify = false;//是否关闭通知，false打开通知推送，true关闭通知推送
 const randomCount = $.isNode() ? 0 : 5;
-let tuanActiveId = `X3vrsg9SABO2P_zyeiqJSw==`;
+let tuanActiveId = `P529ps29ruxRs8jpkoxrQg==`;
 const jxOpenUrl = `openjd://virtual?params=%7B%20%22category%22:%20%22jump%22,%20%22des%22:%20%22m%22,%20%22url%22:%20%22https://wqsd.jd.com/pingou/dream_factory/index.html%22%20%7D`;
 let cookiesArr = [], cookie = '', message = '', allMessage = '';
 const inviteCodes = ['6lw84c1ARwpoRyOtfnF77g==@J1t777njetfQcyEg57lzQA==@W9u_eBl3YKbSjXu0QP3HGQ=@VV55A_oKz5u5CYrL3jxPdg=='
