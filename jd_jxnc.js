7d748caad1653de707b35e1ccfcd9e26f0835465
zero205 <1184642956@qq.com>
2021-03-19 22:39:29 +0800

    fix

--- jd_jxnc.js
+++ jd_jxnc.js
@@ -36,7 +36,7 @@ let currentCookie = ''; // 当前用户 cookie
 let tokenNull = {'farm_jstoken': '', 'phoneid': '', 'timestamp': ''}; // 内置一份空的 token
 let tokenArr = []; // 用户 token 数组
 let currentToken = {}; // 当前用户 token
-let shareCode = '{"smp":"f6611d***9cdd31012ead697f***b30af***29","active":"jdnc_1_1yuanguazi21030***_2","joinnum":1}'; // 内置助力码
+let shareCode = ''; // 内置助力码
 let jxncShareCodeArr = [{"smp":"f6611d***9cdd31012ead697f***b30af***29","active":"jdnc_1_1yuanguazi21030***_2","joinnum":1}]; // 用户 助力码 数组
 let currentShareCode = [{"smp":"f6611d***9cdd31012ead697f***b30af***29","active":"jdnc_1_1yuanguazi21030***_2","joinnum":1}]; // 当前用户 要助力的助力码
 const openUrl = `openjd://virtual?params=${encodeURIComponent('{ "category": "jump", "des": "m", "url": "https://wqsh.jd.com/sns/201912/12/jxnc/detail.html?ptag=7155.9.32&smp=b47f4790d7b2a024e75279f55f6249b9&active=jdnc_1_chelizi1205_2"}',)}`; // 打开京喜农场
