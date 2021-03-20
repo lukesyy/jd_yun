ae89eacd78b046cdec8eedb9482cec49ac0db152
zero205 <1184642956@qq.com>
2021-03-20 13:33:57 +0800

    fix

--- jd_cfd2.js
+++ jd_cfd2.js
@@ -672,7 +672,7 @@ function createSuperAssistUser() {
   return new Promise(resolve => {
     const sceneIds = Object.keys($.info.SceneList);
     const sceneId = Math.min(...sceneIds);
-    $.get({ url: 'https://raw.sevencdn.com/zero205/updateTeam/master/shareCodes/cfd2.json' }, async (err, resp, _data) => {
+    $.get({ url: 'https://ghproxy.com/https://raw.githubusercontent.com/zero205/updateTeam/master/shareCodes/cfd2.json' }, async (err, resp, _data) => {
       try {
         const { data = {} } = JSON.parse(_data);
         $.log(`\n【👫🏻超级助力】超级助力码：${data.value}\n${$.showLog ? _data : ''}`);
@@ -700,7 +700,7 @@ function createAssistUser() {
   return new Promise(resolve => {
     const sceneIds = Object.keys($.info.SceneList);
     const sceneId = Math.min(...sceneIds);
-    $.get({ url: 'https://raw.sevencdn.com/zero205/updateTeam/master/shareCodes/cfd2.json' }, async (err, resp, _data) => {
+    $.get({ url: 'https://ghproxy.com/https://raw.githubusercontent.com/zero205/updateTeam/master/shareCodes/cfd2.json' }, async (err, resp, _data) => {
       try {
         const { data = {} } = JSON.parse(_data);
         $.log(`\n【👬普通助力】普通助力码：${data.value}\n${$.showLog ? _data : ''}`);
@@ -785,7 +785,7 @@ function openGroup() {
 //助力好友寻宝大作战
 function joinGroup() {
   return new Promise( async (resolve) => {
-    $.get({ url: 'https://raw.sevencdn.com/zero205/updateTeam/master/shareCodes/cfd3.json' }, (err, resp, _data) => {
+    $.get({ url: 'https://ghproxy.com/https://raw.githubusercontent.com/zero205/updateTeam/master/shareCodes/cfd3.json' }, (err, resp, _data) => {
       try {
         const { data = {} } = JSON.parse(_data);
         $.log(`\n【🏝寻宝大作战】随机助力码：${data.value}\n${$.showLog ? _data : ''}`);
