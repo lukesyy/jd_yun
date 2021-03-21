a15aa4e6b1f4ca1f07e787602dbd303015cb5832
zero205 <1184642956@qq.com>
2021-03-21 12:04:06 +0800

    修复'nickname' of undefined 错误问题

--- jd_fruit.js
+++ jd_fruit.js
@@ -1385,7 +1385,7 @@ function TotalBean() {
               $.isLogin = false; //cookie过期
               return
             }
-            $.nickName = data['base'].nickname;
+            $.nickName = (data['base'] && data['base'].nickname) || $.UserName;
           } else {
             console.log(`京东服务器返回空数据`)
           }
