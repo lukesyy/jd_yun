fa72a4a0c964d2fc41b8fd63044d4092343e6506
zero205 <1184642956@qq.com>
2021-03-29 02:04:55 +0800

    fix

--- sjys.py
+++ sjys.py
@@ -3,9 +3,9 @@
 # desc: 随机延时，避免同时运行造成任务堵塞
 # license: https://github.com/zero205/JD
 
-print u"执行随机延时，请耐心等待"
+print (u"执行随机延时，请耐心等待")
 import string
 import random
 import time
 time.sleep(random.randint(1,300))
-print u"执行完毕，开始运行脚本"
+print (u"执行完毕，开始运行脚本")
