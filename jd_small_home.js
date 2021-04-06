9e581edf7efb2d6ce374a042c01ceb6c1d586a92
zero205 <1184642956@qq.com>
2021-04-06 21:33:02 +0800

    增加主云函数部署选择-主AC运行，云函数作为补充运行需准点和AC无法运行的脚本

--- jd_small_home.js
+++ jd_small_home.js
@@ -886,7 +886,11 @@ function TotalBean() {
               $.isLogin = false; //cookie过期
               return
             }
-            $.nickName = data['base'].nickname;
+            if (data['retcode'] === 0) {
+              $.nickName = (data['base'] && data['base'].nickname) || $.UserName;
+            } else {
+              $.nickName = $.UserName
+            }
           }
         }
       } catch (e) {
--- jd_unsubscribe.js
+++ jd_unsubscribe.js
@@ -301,7 +301,11 @@ function TotalBean() {
               $.isLogin = false; //cookie过期
               return
             }
-            $.nickName = data['base'].nickname;
+            if (data['retcode'] === 0) {
+              $.nickName = (data['base'] && data['base'].nickname) || $.UserName;
+            } else {
+              $.nickName = $.UserName
+            }
           } else {
             console.log(`京东服务器返回空数据`)
           }
