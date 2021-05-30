21eaeb4a942dedb2b82910fee4ff4d40849042dd
zero205 <1184642956@qq.com>
2021-05-30 22:46:56 +0800

    Update jd_nzmh.js

--- jd_nzmh.js
+++ jd_nzmh.js
@@ -65,7 +65,7 @@ if ($.isNode()) {
       }
       try {
         await jdMh('https://h5.m.jd.com/babelDiy/Zeus/3eeruLXVbXge6CexVq8XkBbBvAfy/index.html')
-        // await jdMh('https://anmp.jd.com/babelDiy/Zeus/36ci7L5uDdhTjqNSNdNiFRbMzAVn/index.html')
+        await jdMh('https://h5.m.jd.com/babelDiy/Zeus/2k7Tx67WzLHDtdDNB2KPnqWEwavX/index.html')
         // await jdMh('https://anmp.jd.com/babelDiy/Zeus/yiNQjMxQvs3R3SdS4nwa2MFk1FE/index.html?wxAppName=jd')
       } catch (e) {
         $.logErr(e)
