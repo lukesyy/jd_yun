5fe3bdd5a641f8c661b5d7cbfef25d60c124acf7
GGHK <85667599+GGHK@users.noreply.github.com>
2021-06-12 16:49:31 +0800

    Update jdCookie.js

--- jdCookie.js
+++ jdCookie.js
@@ -3,7 +3,7 @@
  */
 //此处填写京东账号cookie。
 let CookieJDs = [
-  '',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
+  '',//pt_key=AAJgxHDmADDUFWS-ZoQ4Fh-M5sFouG4L4hYAO5YBUH7yP5XpoQtchHSFnJKpEjiTxNQdwgdMJyQ; pt_pin=15364239578_p; 账号一ck,例:pt_key=XXX;pt_pin=XXX;
   '',//账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
 ]
 // 判断环境变量里面是否有京东ck
