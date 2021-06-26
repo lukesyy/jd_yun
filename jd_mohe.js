88dfe0262a267bdc88964899c2f87b07e8fef8f5
lukesyyy <hh2019@qq.com>
2021-06-27 00:08:15 +0800

    增加东东乐园

--- jd_mohe.js
+++ jd_mohe.js
@@ -37,7 +37,7 @@ if ($.isNode()) {
 
 const JD_API_HOST = 'https://isp5g.m.jd.com';
 //邀请码一天一变化，已确定
-$.shareId = ['cf78fba8-860a-4aed-95a0-ac262c47e41a'];
+$.shareId = ['cde7b048-1cfb-4618-a364-4ae7bb516e7e'];
 !(async () => {
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
