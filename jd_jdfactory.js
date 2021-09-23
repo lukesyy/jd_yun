f88de6ddc1c3c90ba345cae92ceeabcc92da9241
lukeyyyy <hh2019@qq.com>
2021-09-23 10:10:18 +0800

    fix

--- jd_jdfactory.js
+++ jd_jdfactory.js
@@ -54,7 +54,7 @@ let wantProduct = ``;//心仪商品名称
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
 const inviteCodes = ['T0225KkcRU8Y9FaEIh_3wPAKcQCjVWnYaS5kRrbA'];
 let myInviteCode;
-$.newShareCode = [];
+$.newShareCode = ['T0225KkcRU8Y9FaEIh_3wPAKcQCjVWnYaS5kRrbA'];
 !(async () => {
   await requireConfig();
   if (!cookiesArr[0]) {
