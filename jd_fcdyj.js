82d13fdb31344a927ba1e92f765b7845f13c292d
lukesyyy <hh2019@qq.com>
2021-10-01 10:37:03 +0800

    FIX

--- jd_fcdyj.js
+++ jd_fcdyj.js
@@ -30,8 +30,8 @@ const jdCookieNode = $.isNode() ? require('./jdCookie.js') : ''
 const dyjCode = $.isNode()
 	? process.env.dyjCode
 		? process.env.dyjCode
-		: '7343a7abb7e64f69906d1a70a75e4b4060641632931222752@TLkD8_PycMVws_9HtL2YLxaNNJ_CYAljNZtiKqj2jvI'
-	: '7343a7abb7e64f69906d1a70a75e4b4060641632931222752@TLkD8_PycMVws_9HtL2YLxaNNJ_CYAljNZtiKqj2jvI' //邀请码变量，不支持多账号，格式：redEnvelopeId@markedPin
+		: '8128d43ea77e46af9733ea062943563e72871633017608034@TLkD8_PycMVws_9HtL2YLxaNNJ_CYAljNZtiKqj2jvI'
+	: '8128d43ea77e46af9733ea062943563e72871633017608034@TLkD8_PycMVws_9HtL2YLxaNNJ_CYAljNZtiKqj2jvI' //邀请码变量，不支持多账号，格式：redEnvelopeId@markedPin
 const myCode = [
 	{
 		rid: '182b42989fae4f2fbd043561c09ca51855151632240019136',
