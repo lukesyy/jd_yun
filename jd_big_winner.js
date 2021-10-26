23ab30b363f8a900227b5ece1dc8dd5bd5b94803
lukeyyyy <hh2019@qq.com>
2021-10-26 09:40:16 +0800

    大赢家回归

--- jd_big_winner.js
+++ jd_big_winner.js
@@ -7,14 +7,11 @@
 [task_local]
 #省钱大赢家之翻翻乐
 20 0,6-23 * * * jd_big_winner.js, tag=省钱大赢家之翻翻乐, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
-
 =================================Loon===================================
 [Script]
 cron "20 0,6-23 * * *" script-path=jd_big_winner.js,tag=省钱大赢家之翻翻乐
-
 ===================================Surge================================
 省钱大赢家之翻翻乐 = type=cron,cronexp="20 0,6-23 * * *",wake-system=1,timeout=3600,script-path=jd_big_winner.js
-
 ====================================小火箭=============================
 省钱大赢家之翻翻乐 = type=cron,script-path=jd_big_winner.js, cronexpr="20 0,6-23 * * *", timeout=3600, enable=true
  */
@@ -23,7 +20,7 @@ const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 //IOS等用户直接用NobyDa的jd cookie
-let cookiesArr = [], cookie = '', message = '', linkId = ' yMVR-_QKRd2Mq27xguJG-w', fflLinkId = 'YhCkrVusBVa_O2K-7xE6hA';
+let cookiesArr = [], cookie = '', message = '', linkId = 'PFbUR7wtwUcQ860Sn8WRfw', fflLinkId = 'YhCkrVusBVa_O2K-7xE6hA';
 const JD_API_HOST = 'https://api.m.jd.com/api';
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
