6eeba063d9fbd7bd61d1e866c2c5e9976821e597
lukeyyyy <hh2019@qq.com>
2021-11-29 09:58:11 +0800

    fix

--- jd_mohe.js
+++ jd_mohe.js
@@ -70,6 +70,7 @@ $.shareId = [];
       await Promise.all([
         task0()
       ])
+      $.taskList_limit = 0
       await taskList();
       await getAward();//抽奖
     }
@@ -217,6 +218,7 @@ function getCoin() {
 }
 
 async function taskList() {
+  $.taskList_limit++
   return new Promise(async (resolve) => {
     const body = {"apiMapping":"/active/taskList"}
     $.post(taskurl(body), async (err, resp, data) => {
@@ -254,8 +256,12 @@ async function taskList() {
             console.log('\n\n----taskList的任务全部做完了---\n\n')
             console.log(`分享好友助力 ${task5.finishNum}/${task5.totalNum}\n\n`)
           } else {
-            console.log(`请继续等待,正在做任务,不要退出哦`)
-            await taskList();
+            if ($.taskList_limit >= 15){
+              console.log('触发死循环保护,结束')
+            } else {
+              console.log(`请继续等待,正在做任务,不要退出哦`)
+              await taskList();
+            }
           }
         }
       } catch (e) {
