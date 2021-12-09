7a1ff5abc4c1d8c9805a82e07b57a121c3cfbd11
lukeyyyy <hh2019@qq.com>
2021-12-09 10:04:01 +0800

    京魔方兑换

--- jd_cfd_pearl_ex.js
+++ jd_cfd_pearl_ex.js
@@ -86,6 +86,7 @@ async function perl_auto() {
       }
       return flag
     })
+	await wait()
     if (!prizes.length) {
       console.log('无红包满足条件,结束')
       return
@@ -104,7 +105,6 @@ async function perl_auto() {
       })
     }
     // console.debug('prizes:',prizes)
-    await wait()
     for (let i = 0; i < prizes.length; i++) {
       const prize = prizes[i]
       console.log('兑换面额:', prize.strPrizeName || '随机红包')
--- jd_super_redrain.js
+++ jd_super_redrain.js
@@ -1,24 +1,31 @@
 /*
  * @Date: 2021-08-25 14:02:18
  * @LastEditors: LiJinGang
- * @LastEditTime: 2021-11-09 10:00:58
+ * @LastEditTime: 2021-12-09 10:01:24
  */
 /*
 整点京豆雨，每天8*16豆
+
 boxjs订阅地址: https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/longzhuzhu.boxjs.json
+
 环境变量:
 # 关闭京豆雨通知
 export RAIN_NOTIFY_CONTROL="false"
+
 已支持IOS双京东账号,Node.js支持N个京东账号
 脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
+
 [task_local]
 #整点京豆雨
 3 0-23/1 * * * https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/jd_super_redrain.js, tag=整点京豆雨, enabled=true
+
 ================Loon==============
 [Script]
 cron "3 0-23/1 * * *" script-path=https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/jd_super_redrain.js,tag=整点京豆雨
+
 ===============Surge=================
 整点京豆雨 = type=cron,cronexp="3 0-23/1 * * *",wake-system=1,timeout=20,script-path=https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/jd_super_redrain.js
+
 ============小火箭=========
 整点京豆雨= type=cron,script-path=https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/jd_super_redrain.js, cronexpr="3 0-23/1 * * *",timeout=200, enable=true
  */
